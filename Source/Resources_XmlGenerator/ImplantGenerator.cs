9a208028c27d6aaf431c82d4e20e54afbc7b10b0
WVCSergkart <114881826+WVCSergkart@users.noreply.github.com>
2023-11-20 23:21:27 +0300

    v0.98.70_GeneratorFixes

--- Source/Resources_XmlGenerator/ImplantGenerator.cs
+++ Source/Resources_XmlGenerator/ImplantGenerator.cs
@@ -17,40 +17,44 @@ namespace WVC_UltraExpansion
 
 		static PostInitializationDefGenerator()
 		{
-			foreach (ImplantGeneratorDef generatorDef in DefDatabase<ImplantGeneratorDef>.AllDefsListForReading)
+			bool onOrOff = false;
+			if (onOrOff)
 			{
-				string name = "DeleteTag";
-				XDocument xDocument1 = new(new XElement("Defs"));
-				XDocument xDocument2 = new(new XElement("Defs"));
-				XDocument xDocument3 = new(new XElement("Defs"));
-				xDocument1.Element("Defs").SetAttributeValue(name, "DeleteImplant_" + generatorDef.defName);
-				xDocument2.Element("Defs").SetAttributeValue(name, "DeleteImplant_" + generatorDef.defName);
-				xDocument3.Element("Defs").SetAttributeValue(name, "DeleteImplant_" + generatorDef.defName);
-				TemplatesUtility.GetOrCreateStorageModFolders(generatorDef, out string path);
-				TemplatesUtility.GetOrCreateStorageModFolders(generatorDef, out string path1);
-				TemplatesUtility.GetOrCreateStorageModFolders(generatorDef, out string path2);
-				foreach (BodyPartDef bodyPartDef in generatorDef.bodyPartDefs)
+				TemplatesUtility.GetOrCreateStorageModFolders(out string path);
+				TemplatesUtility.GetOrCreateStorageModFolders(out string path1);
+				TemplatesUtility.GetOrCreateStorageModFolders(out string path2);
+				foreach (ImplantGeneratorDef generatorDef in DefDatabase<ImplantGeneratorDef>.AllDefsListForReading)
 				{
-					TemplatesUtility.GetFromTemplate_ThingDef(generatorDef.thingTemplateDef, bodyPartDef, generatorDef);
+					// string name = "DeleteTag";
+					XDocument xDocument1 = new(new XElement("Defs"));
+					XDocument xDocument2 = new(new XElement("Defs"));
+					XDocument xDocument3 = new(new XElement("Defs"));
+					// xDocument1.Element("Defs").SetAttributeValue(name, "DeleteImplant_" + generatorDef.defName);
+					// xDocument2.Element("Defs").SetAttributeValue(name, "DeleteImplant_" + generatorDef.defName);
+					// xDocument3.Element("Defs").SetAttributeValue(name, "DeleteImplant_" + generatorDef.defName);
+					foreach (BodyPartDef bodyPartDef in generatorDef.bodyPartDefs)
+					{
+						TemplatesUtility.GetFromTemplate_ThingDef(generatorDef.thingTemplateDef, bodyPartDef, generatorDef);
+					}
+					foreach (HediffDef hediffDef in hediffDefs)
+					{
+						xDocument1.Element("Defs").Add(TemplatesUtility.GenerateHediffDefFile(hediffDef, generatorDef));
+					}
+					foreach (RecipeDef recipeDef in recipeDefs)
+					{
+						xDocument2.Element("Defs").Add(TemplatesUtility.GenerateSurgeryDefFile(recipeDef));
+					}
+					foreach (ThingDef thingDef in thingDefs)
+					{
+						xDocument3.Element("Defs").Add(TemplatesUtility.GenerateThingDefFile(thingDef, generatorDef));
+					}
+					xDocument1.Save(Path.Combine(path, "HediffDef_" + generatorDef.defName + ".xml"));
+					xDocument2.Save(Path.Combine(path1, "RecipeDef_" + generatorDef.defName + ".xml"));
+					xDocument3.Save(Path.Combine(path2, "ThingDef_" + generatorDef.defName + ".xml"));
+					thingDefs.Clear();
+					recipeDefs.Clear();
+					hediffDefs.Clear();
 				}
-				foreach (HediffDef hediffDef in hediffDefs)
-				{
-					xDocument1.Element("Defs").Add(TemplatesUtility.GenerateHediffDefFile(hediffDef, generatorDef));
-				}
-				foreach (RecipeDef recipeDef in recipeDefs)
-				{
-					xDocument2.Element("Defs").Add(TemplatesUtility.GenerateSurgeryDefFile(recipeDef));
-				}
-				foreach (ThingDef thingDef in thingDefs)
-				{
-					xDocument3.Element("Defs").Add(TemplatesUtility.GenerateThingDefFile(thingDef, generatorDef));
-				}
-				xDocument1.Save(Path.Combine(path, "HediffDef_" + generatorDef.defName + ".xml"));
-				xDocument2.Save(Path.Combine(path1, "RecipeDef_" + generatorDef.defName + ".xml"));
-				xDocument3.Save(Path.Combine(path2, "ThingDef_" + generatorDef.defName + ".xml"));
-				thingDefs.Clear();
-				recipeDefs.Clear();
-				hediffDefs.Clear();
 			}
 		}
 	}
